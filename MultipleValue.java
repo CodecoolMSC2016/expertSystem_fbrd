81f878cc4d91a5211c44e0385aea89c79cafade7
imre_meszesan <imre.meszesan@gmail.com>
2017-02-15 10:55:00 +0100

    added getInputPattern method

--- MultipleValue.java
+++ MultipleValue.java
@@ -2,6 +2,11 @@ public class MultipleValue implements Value {
     private String param;
 
     public MultipleValue(String param){
+
         this.param = param;
+    }
+
+    public String[] getInputPattern(){
+        return null;
     }
 }
--- SingleValue.java
+++ SingleValue.java
@@ -2,6 +2,11 @@ public class SingleValue implements Value {
     private String param;
 
     public SingleValue(String param){
+
         param = param;
+    }
+
+    public String[] getInputPattern(){
+        return null;
     }
 }
