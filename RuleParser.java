27d77dfcac1c5f7bad366cb6af81b630a37e6b4e
imre_meszesan <imre.meszesan@gmail.com>
2017-02-15 10:57:52 +0100

    added main method for testing, created Document from xml in loadXml... method

--- RuleParser.java
+++ RuleParser.java
@@ -1,8 +1,34 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class RuleParser implements XmlParser{
+
+    public void loadXmlDocument(String fullPath){
+        try {
+            File inputFile = new File(fullPath);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(inputFile);
+            document.getDocumentElement().normalize();
+            System.out.println(document.getDocumentElement().getNodeName());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 
     public RuleRepository getRuleRepository(){
 
         return null;
+    }
+
+    public static void main(String[] args){
+        RuleParser ruleParser = new RuleParser();
+        ruleParser.loadXmlDocument("src/rules.xml");
     }
 
 }
