4c630dd7492f4fbeaf32dc2ff638a148903dd4c8
imre_meszesan <imre.meszesan@gmail.com>
2017-02-16 14:24:50 +0100

    implemented evaluateAnswerByInput

--- Answer.java
+++ Answer.java
@@ -1,10 +1,23 @@
 public class Answer {
+    private Value valueTrue;
+    private Value valueFalse;
 
-    public boolean evaluateAnserbyInput(String input){
-        return false;
+    public Boolean evaluateAnserbyInput(String input){
+        for (String value : valueTrue.getInputPattern()){
+            if (input.equals(value)){
+                return true;
+            }
+        }
+        for (String value : valueFalse.getInputPattern()){
+            if (input.equals(value)){
+                return false;
+            }
+        }
+        return null;
     }
 
-    public void addValue(Value value){
-
+    public void addValue(Value valueTrue, Value valueFalse){
+        this.valueTrue = valueTrue;
+        this.valueFalse = valueFalse;
     }
 }
