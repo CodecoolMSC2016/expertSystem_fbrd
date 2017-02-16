2ca8fa75e60fa5b6d2d2397a21b47c621b3bbac6
imre_meszesan <imre.meszesan@gmail.com>
2017-02-16 11:17:22 +0100

    implemented QuestionIterator inner class

--- RuleRepository.java
+++ RuleRepository.java
@@ -4,41 +4,52 @@ import java.util.Map;
 public class RuleRepository {
 	private Map<String, String[][]> valueMap;
 	private Map<String, String> questionMap;
+	private Question[] questionArray;
+	private QuestionIterator questionIterator;
 
 
 	public RuleRepository(Map<String, String> questionMap, Map<String, String[][]> valueMap){
 		this.valueMap = valueMap;
 		this.questionMap = questionMap;
-		QuestionIterator questionIterator = new QuestionIterator();
+		questionArray = new Question[questionMap.size()];
+		questionIterator = new QuestionIterator();
+	}
+
+	public void createQuestionArray(){
+		int counter = 0;
+		for (Map.Entry entry: questionMap.entrySet()){
+			questionArray[counter] = new Question((String) entry.getKey());
+			counter++;
+		}
 	}
 	private class QuestionIterator implements Iterator{
-
-		public QuestionIterator(){
-
-		}
+		int index;
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+			if (index < questionMap.size()){
+				return true;
+			}else {
+				return false;
+			}
 		}
 
 		@Override
 		public Object next() {
-			// TODO Auto-generated method stub
+			if (this.hasNext()){
+				return questionArray[index++];
+			}
 			return null;
 		}
 
 	}
 
-	public Question addQuestion(String id, Question question){
-
+	public Question addQuestion(String id, String question){
 		return null;
 	}
 
 	public Iterator getIterator(){
-
-		return null;
+		return questionIterator;
 	}
 
 }
