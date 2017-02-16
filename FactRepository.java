a129babfe321e12090372fb6e43a3a9781e58a3a
redgyuf <faramirex070@gmail.com>
2017-02-16 14:32:27 +0100

    implemented setEvalsToFact

--- FactRepository.java
+++ FactRepository.java
@@ -1,8 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class FactRepository {
-	private Fact[] factIDs;
+	private Fact[] facts;
 	private HashMap<String,String> factMap;
 	private HashMap<String, HashMap<String,Boolean>> factEvalMap;
 	private FactIterator factIterator;
@@ -10,7 +11,7 @@ public class FactRepository {
 	public FactRepository(HashMap<String,String> factMap, HashMap<String, HashMap<String,Boolean>> factEvalMap){
 		this.factMap = factMap;
 		this.factEvalMap = factEvalMap;
-		factIDs = new Fact[factMap.size()];
+		facts = new Fact[factMap.size()];
 		factIterator = new FactIterator();
 	}
 	
@@ -18,9 +19,21 @@ public class FactRepository {
 		int index = 0;
 		for(Map.Entry entry: factMap.entrySet()){
 			Fact fact = new Fact((String) entry.getValue());
-			factIDs[index] = fact;
+			facts[index] = fact;
 			fact.setID((String) entry.getKey());
 			index++;
+		}
+	}
+	
+	public void setEvalsToFact(){
+		for(int i = 0; i < facts.length; i++){
+			String factID = facts[i].getID();
+			HashMap<String,Boolean> evalMap = factEvalMap.get(factID);
+			Set<String> evalIDs = evalMap.keySet();
+			
+			for(String ID : evalIDs){
+				facts[i].setFactValueByID(ID, evalMap.get(ID).equals("true"));				
+			}
 		}
 	}
 
@@ -28,7 +41,7 @@ public class FactRepository {
 		int index;
 		@Override
 		public boolean hasNext() {
-			if(index < factIDs.length){
+			if(index < facts.length){
 	            return true;
 	         }
 	         return false;
@@ -37,7 +50,7 @@ public class FactRepository {
 		@Override
 		public Object next() {
 			if(this.hasNext()){
-	            return factIDs[index++];
+	            return facts[index++];
 	         }
 	         return null;
 		}
@@ -46,6 +59,10 @@ public class FactRepository {
 
     public Iterator getIterator(){
     	return factIterator;
+    }
+    
+    public Fact[] getFacts(){
+    	return facts;
     }
 
     public void addFact(Fact fact){
